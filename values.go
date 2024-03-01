6d56a844c0b1bf6f5033d77723af5b3de77997fe
Aman <akapur07@gmail.com>
2024-03-02 01:10:02 +0530

    Values

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string
+	fmt.Println("Hello " + "World")
+	//Numbers
+	fmt.Println("1+1=", 1+1)
+	//logical operarator
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
